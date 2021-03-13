d033aae0f3cc25bf5973d49e3124e13b906875b1
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-13 22:36:01 +0530

    updated admin panel

--- base/admin.py
+++ base/admin.py
@@ -1,11 +1,27 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title', 'complete', 'created_on', 'udpated_on')
-    list_display_links = ('user', 'title', 'complete',
+    list_display = ('user', 'title', 'colored_status',
+                    'created_on', 'udpated_on')
+    list_display_links = ('user', 'title', 'colored_status',
                           'created_on', 'udpated_on')
+    date_hierarchy = 'created_on'
+    list_filter = ('complete', 'user')
+
+    def colored_status(self, obj):
+        if obj.complete:
+            return format_html(
+                '<span style="color: green;">{}</span>',
+                (obj.complete),
+            )
+        else:
+            return format_html(
+                '<span style="color: red;">{}</span>',
+                (obj.complete),
+            )
 
 
 
