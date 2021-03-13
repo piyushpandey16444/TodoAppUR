be7dda7f574c320eab2369fcb2f2e047d6d5ca11
piyushpandey16444@gmail.com <piyushpandey16444@gmail.com>
2021-03-13 22:23:44 +0530

    models created

--- base/models.py
+++ base/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=250)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    udpated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['complete']
